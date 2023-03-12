217bf48704e0dcfc5d3173dfcc045ac88b62c418
jan_s+git@posteo.de <jan_s+github@posteo.de>
2023-03-12 22:16:37 +0100

    header from listings5

--- vec.go
+++ vec.go
@@ -28,12 +28,20 @@ func (v *vec3) Neg() {
 	v.e2 *= -1
 }
 
-func (v vec3) Add(vv vec3) vec3 {
+func Add(v vec3, vv vec3) vec3 {
 	return vec3{v.e0 + vv.e0, v.e1 + vv.e1, v.e2 + vv.e2}
+}
+
+func Sub(v vec3, vv vec3) vec3 {
+	return vec3{v.e0 - vv.e0, v.e1 - vv.e1, v.e2 - vv.e2}
 }
 
 func (v vec3) Multi(t float64) vec3 {
 	return vec3{v.e0 * t, v.e1 * t, v.e2 * t}
+}
+
+func Multi(v vec3, vv vec3) vec3 {
+	return vec3{v.e0 * vv.e0, v.e1 * vv.e1, v.e2 * vv.e2}
 }
 
 func (v vec3) Div(t float64) vec3 {
@@ -46,4 +54,18 @@ func (v vec3) LenSquared() float64 {
 
 func (v vec3) Len() float64 {
 	return float64(math.Sqrt(float64(v.LenSquared())))
+}
+
+func Dot(v vec3, vv vec3) float64 {
+	return v.e0*vv.e0 + v.e1 + vv.e1 + v.e2 + vv.e2
+}
+
+func Cross(v vec3, vv vec3) vec3 {
+	return vec3{v.e1*vv.e2 - v.e2*vv.e1,
+		v.e2*vv.e0 - v.e0*vv.e2,
+		v.e0*vv.e1 - v.e1*vv.e0}
+}
+
+func UnitVector(v vec3) vec3 {
+	return v.Div(v.Len())
 }
